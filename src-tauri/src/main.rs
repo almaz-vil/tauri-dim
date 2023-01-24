9c11015fe15acd4fdf4db22a0f4dc0bc44f9fa57
almaz-vil <37326184+almaz-vil@users.noreply.github.com>
2023-01-24 14:31:21 +0300

    Add files via upload

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,9 +6,31 @@ extern crate page_size;
 use std::{path::PathBuf, fs::File, io::{BufReader, BufRead}};
 use std::fmt;
 use std::f32;
-
+use std::str::FromStr;
 use tauri::Manager;
 
+
+#[derive(Clone)]
+struct LoadAvg{
+    loadm: f32,
+    loadm_5: f32,
+    loadm_15: f32,
+    
+}
+
+impl std::fmt::Debug for LoadAvg {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut s=String::new();
+        s.push_str("{\"loadm\":\"");
+        s.push_str(&self.loadm.to_string());
+        s.push_str("\", \"loadm5\":\"");
+        s.push_str(&self.loadm_5.to_string());
+        s.push_str("\", \"loadm15\":\"");
+        s.push_str(&self.loadm_15.to_string());
+        s.push_str("\"}");
+        f.write_str(s.as_str())         
+    }
+}
 #[derive(Clone,  PartialEq, PartialOrd)]
 struct InfoMem{
     total: f32,
@@ -38,12 +60,76 @@ struct InfoProc{
     name: String,
     state: String,
     vmsize: f32,
+    loadcpu: f64,
     error: String,
 }
 
 impl InfoProc {
     pub fn kb_in_mb(&mut self){
         self.vmsize=self.vmsize/1024.00;
+    }
+    pub fn loadcpu(&mut self, namefile: String){
+        let mut sysytem_uptime_sec=0.0;
+        let f_stat = match File::open("/proc/uptime"){
+            Ok(x)=>x,
+            Err(e)=> {self.error=e.to_string(); return}
+        };
+        let buf_smaps=BufReader::new(f_stat);
+        let iter_s=buf_smaps.lines();
+        for line_result in iter_s {
+            let line =match line_result{
+                Ok(x)=> x,
+                Err(_)=>"-".to_string()
+            };
+            let mut s_p=line.split_whitespace();
+            let uptime=s_p.nth(0).unwrap();
+            sysytem_uptime_sec= match f64::from_str(uptime){
+                Ok(x)=>x as f64,
+                Err(_)=>0.0
+            };
+        }
+
+        let f_stat = match File::open(namefile+"/stat"){
+            Ok(x)=>x,
+            Err(e)=> {self.error=e.to_string(); return}
+        };
+        let buf_smaps=BufReader::new(f_stat);
+        let iter_s=buf_smaps.lines();
+        for line_result in iter_s {
+            let line =match line_result{
+                Ok(x)=> x,
+                Err(_)=>"-".to_string()
+            };
+            let  stat=line.split_whitespace();
+            let mut il=0;
+            let mut utimm="0";
+            let mut stimm="0";
+            let mut process_starttime="0";
+            for s in stat.clone()  {
+                match il {
+                    13=>utimm=s,
+                    14=>stimm=s,
+                    21=>process_starttime=s,
+                    _=>()
+                }
+                il=il+1;
+
+            }
+            let ut=100.00  ;
+            let utime=f64::from_str(utimm).unwrap();
+            let stime=f64::from_str(stimm).unwrap();
+            let startime=f64::from_str(process_starttime).unwrap();
+            let process_utime_sec=utime/ut;
+            let process_stime_sec=stime/ut;
+            let process_startime_sec=startime/ut;
+            let process_elapsed_sec=sysytem_uptime_sec-process_startime_sec;
+            let process_usage_sec=process_utime_sec+process_stime_sec;
+            let mut process_usage=1.00;
+            if process_elapsed_sec>0.0 {
+            process_usage=process_usage_sec*100.00/process_elapsed_sec;}
+            self.loadcpu=process_usage;
+            
+        }
     }
 }
 
@@ -56,6 +142,8 @@ impl std::fmt::Debug for InfoProc {
         s.push_str(&self.name);
         s.push_str("\", \"state\":\"");
         s.push_str(&self.state);
+        s.push_str("\", \"loadcpu\":\"");
+        s.push_str(&self.loadcpu.floor().to_string());
         s.push_str("\", \"vmsize\":\"");
         s.push_str(&self.vmsize.to_string());
         s.push_str("\", \"error\":\"");
@@ -63,6 +151,27 @@ impl std::fmt::Debug for InfoProc {
         s.push_str("\"}");    
         f.write_str(s.as_str())        
     }
+}
+
+#[tauri::command]
+fn loadcpu()->String{
+    let mut loadcpu= LoadAvg{
+        loadm: 0.0,
+        loadm_5: 0.0,
+        loadm_15: 0.0,
+    };
+    let f_smaps = File::open("/proc/loadavg").unwrap();
+    let buf_smaps=BufReader::new(f_smaps);
+    let mut iter_s=buf_smaps.lines();
+    let line= match iter_s.next() {
+        Some(l) => l.unwrap(),
+        None => return format!("{:?}", loadcpu)
+    };
+    let v: Vec<&str>=line.split(" ").collect();
+    loadcpu.loadm=v[0].parse::<f32>().unwrap();
+    loadcpu.loadm_5=v[1].parse::<f32>().unwrap();
+    loadcpu.loadm_15=v[2].parse::<f32>().unwrap();
+    format!("{:?}", loadcpu)
 }
 
 #[tauri::command]
@@ -101,8 +210,8 @@ fn mem()->String{
 }
 
 #[tauri::command]
-fn ver()->String{
-    let home = "/proc/";
+fn ver(insort: i32)->String{
+let home = "/proc/";
 let mut path = PathBuf::new();
 path.push(home);
 let rd= match path.read_dir(){
@@ -116,6 +225,7 @@ for entry_result in rd{
         name: " ".to_string(),
         state: " ".to_string(),
         vmsize: 0.0,
+        loadcpu:0.0,
         error: " ".to_string()
     };
     let entry=entry_result.unwrap();
@@ -124,7 +234,8 @@ for entry_result in rd{
         Ok(x)=>x,
         _=>continue
     };
-    let f = match File::open(entry.path().to_string_lossy().to_string()+"/status"){
+    let n_file=entry.path().to_string_lossy().into_owned().as_str().to_string();
+    let f = match File::open(n_file.clone()+"/status"){
         Ok(x)=>x,
         Err(e)=>{
             var_info.error=e.to_string();
@@ -134,7 +245,7 @@ for entry_result in rd{
     let buf=BufReader::new(f);
     let mut iter=buf.lines();
     var_info.name=iter.next().unwrap().ok().unwrap()[5..].to_string().trim().to_string();
-    let f_smaps = match File::open(entry.path().to_string_lossy().to_string()+"/status"){
+    let f_smaps = match File::open(n_file.clone()+"/status"){
         Ok(x)=>x,
         Err(e)=>{
             var_info.error=e.to_string();
@@ -161,9 +272,16 @@ for entry_result in rd{
         }
     }
     var_info.kb_in_mb();
+    var_info.loadcpu(n_file.clone());
     list.push(var_info);    
 };
-list.sort_by(|b, a| {a.vmsize.total_cmp(&b.vmsize)});
+match insort {
+  0=>{list.sort_by(|a, b| {a.id.cmp(&b.id)});} ,
+  1=>{list.sort_by(|a, b| {a.name.cmp(&b.name)});},
+  3=>{list.sort_by(|b, a| {a.loadcpu.total_cmp(&b.loadcpu)});},
+  _=> {list.sort_by(|b, a| {a.vmsize.total_cmp(&b.vmsize)});}
+}
+
 format!("{:?}", list)
 }
 
@@ -173,10 +291,10 @@ fn main() {
     .setup(|app| {
         let win = app.get_window("main").unwrap();
         win.show().unwrap();
-        win.open_devtools();
+       // win.open_devtools();
         Ok(())}
         )
-    .invoke_handler(tauri::generate_handler![ver, mem])
+    .invoke_handler(tauri::generate_handler![ver, mem, loadcpu])
     .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -25,23 +25,20 @@ class graf{
       ctx.height=this.h;
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, this.w, this.h);
-      
       for (let index = 0; index < this.lines.length; index++) {
         this.lines[index]=0;      
-      }
-        
-    
-      this.count=1;};
+      }      
+      this.count=1;
+    };
     this.lines[this.count]=y;
     let ctx=this.canvas.getContext('2d');
     ctx.wigth=this.w;
     ctx.height=this.h;
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#F0E68C";
     ctx.fillRect(0, 0, this.w, this.h);
-    
     for (let index = 0; index < this.lines.length; index++) {
       const y_e = this.lines[index];
-      ctx.strokeStyle="blue";
+      ctx.strokeStyle="#32CD32";
       ctx.moveTo(index*this.wlines,this.h-y_e);
       ctx.lineTo(index*this.wlines,this.h);
       ctx.lineWidth=this.wlines;
@@ -53,7 +50,7 @@ class graf{
 
 }
 
-
+let procInfoSum;
 let greetInputEl;
 let greetMsgEl;
 let versionMsgEl;
@@ -63,6 +60,9 @@ let cheskRun = false;
 let printt=new Intl.NumberFormat('ru-RU');
 let startcraf=true;
 let craf;
+let msgLoadCPU;
+
+let paramsort=3;
 
 async function start() {
   interval=setInterval(version, 1000);
@@ -77,55 +77,84 @@ function stoptimer(){
   document.querySelector("#version-button").style.display = 'block';   
 }
 
+async function loadcpu(){
+  let loadcpu= await invoke("loadcpu");
+  let obj = JSON.parse(loadcpu);
+  if(obj.loadm>obj.loadm5){
+    if(obj.loadm5>obj.loadm15){
+      msgLoadCPU.innerHTML=` 1 мин: ${obj.loadm} 5 мин: <b>${obj.loadm5}</b> 15 мин: ${obj.loadm15}`;
+      if(obj.loadm>obj.loadm5){
+        msgLoadCPU.innerHTML=` 1 мин: <b>${obj.loadm}</b> 5 мин: <b>${obj.loadm5}</b> 15 мин: ${obj.loadm15}`;
+      }
+    }else{
+      msgLoadCPU.innerHTML=` 1 мин: <b>${obj.loadm}</b> 5 мин: ${obj.loadm5} 15 мин: ${obj.loadm15}`;
+    }
+  }else{
+  msgLoadCPU.innerHTML=` 1 мин: ${obj.loadm} 5 мин: ${obj.loadm5} 15 мин: ${obj.loadm15}`;
+  }
+}
+
 async function meminfo() {
   let mem=await invoke("mem");
   let obj=JSON.parse(mem);
-  greetMsgEl.innerHTML=`Всего памяти: ${printt.format(obj.total)} Mb. Занято: ${printt.format(obj.total-obj.free)} Mb. Cвободно: ${printt.format(obj.free)} Mb.`;
+  let mem_used=obj.total-obj.free;
+  if (obj.free<mem_used){
+    greetMsgEl.innerHTML=`Всего памяти: ${printt.format(obj.total)}&nbsp;Mb.<br>Занято: <b>${printt.format(obj.total-obj.free)}</b>&nbsp;Mb.<br>Cвободно: ${printt.format(obj.free)}&nbsp;Mb.`;
+  }else{
+    greetMsgEl.innerHTML=`Всего памяти: ${printt.format(obj.total)}&nbsp;Mb.<br>Занято: ${printt.format(obj.total-obj.free)}&nbsp;Mb.<br>Cвободно: ${printt.format(obj.free)}&nbsp;Mb.`;
+  }
+
   if (startcraf){
     startcraf=false;
     let canvas= document.querySelector('#canvas-id');
     craf = new graf(canvas.style.height, canvas.style.wight, obj.total, canvas);
    } else{
-    craf.Pix(obj.total-obj.free);
+    craf.Pix(mem_used);
    }
 }
 
+function frop(s) {
+  console.log("{}", s);
+}
+
 async function version() {
-  let jso=await invoke("ver");
+  let jso=await invoke("ver", {insort: paramsort});
   let mas = jso.split(/{([\s\S]+?)}/);
   let s=
   `<div style="text-shadow: none; background-color: aliceblue;  height: 80vh; overflow:auto;" id="log_error" >`+
   `<table>
-    <tr><th>ID</th><th>Имя</th><th>Размер в памяти</th><th>Cостояние</th></tr>
+    <tr><th onclick="paramsort=0;' ">ID</th><th onclick="paramsort=1;">Имя</th><th onclick="paramsort=2;">Размер в памяти</th><th onclick="paramsort=3;">CPU</th><th onclick="paramsort=4;">Cостояние</th></tr>
     `
   let count_vmsime=0;
+  let count_cpu=0;
   let text=greetInputEl.value;
   mas.forEach(function(item, index, array) {
     if (item.includes("id")){
       let d=`{${item}}`;
       let obj=JSON.parse(d);
       count_vmsime=count_vmsime+parseFloat(obj.vmsize,10);
+      count_cpu=count_cpu+parseInt(obj.loadcpu);
       if (text.length>0){
         if (obj.name.includes(text)){
           if (obj.state.includes("R")){
-            s=s+`<tr class="run" ><td>${obj.id}</td><td>${obj.name}</td><td>${printt.format(obj.vmsize)} Mb.</td><td>${obj.state}</td></tr>`;
+            s=s+`<tr class="run" ><td>${obj.id}</td><td>${obj.name}</td><td>${printt.format(obj.vmsize)} Mb.</td><td>${printt.format(obj.loadcpu)}%</td><td>${obj.state}</td></tr>`;
           }
           else{
-            s=s+`<tr><td>${obj.id}</td><td>${obj.name}</td><td>${printt.format(obj.vmsize)} Mb.</td><td>${obj.state}</td></tr>`;
+            s=s+`<tr><td>${obj.id}</td><td>${obj.name}</td><td>${printt.format(obj.vmsize)} Mb.</td><td>${printt.format(obj.loadcpu)}%</td><td>${obj.state}</td></tr>`;
           }     
         }
       }
       else {
       if (obj.state.includes("R")){
-        s=s+`<tr class="run" ><td>${obj.id}</td><td>${obj.name}</td><td>${new Intl.NumberFormat('ru-RU').format(obj.vmsize)} Mb.</td><td>${obj.state}</td></tr>`;
+        s=s+`<tr class="run" ><td>${obj.id}</td><td>${obj.name}</td><td>${new Intl.NumberFormat('ru-RU').format(obj.vmsize)} Mb.</td><td>${printt.format(obj.loadcpu)}%</td><td>${obj.state}</td></tr>`;
       }
       else{
         if (cheskRun.checked){
           if (obj.state.includes("R")){
-            s=s+`<tr class="run" ><td>${obj.id}</td><td>${obj.name}</td><td>${printt.format(obj.vmsize)} Mb.</td><td>${obj.state}</td></tr>`;
+            s=s+`<tr class="run" ><td>${obj.id}</td><td>${obj.name}</td><td>${printt.format(obj.vmsize)} Mb.</td><td>${printt.format(obj.loadcpu)}%</td><td>${obj.state}</td></tr>`;
           }
         } else {
-        s=s+`<tr><td>${obj.id}</td><td>${obj.name}</td><td>${new Intl.NumberFormat('ru-RU').format(obj.vmsize)} Mb.</td><td>${obj.state}</td></tr>`;
+        s=s+`<tr><td>${obj.id}</td><td>${obj.name}</td><td>${new Intl.NumberFormat('ru-RU').format(obj.vmsize)} Mb.</td><td>${printt.format(obj.loadcpu)}%</td><td>${obj.state}</td></tr>`;
         
         }
       }
@@ -134,17 +163,23 @@ async function version() {
   }
     
   });
-  s=s+'</table></div>'
-  versionMsgEl.innerHTML=`Всего занято ${mas.length} процессами ${new Intl.NumberFormat('ru-RU').format(count_vmsime)} Mb. памяти. ${s}`;
-
+  s=s+'</table></div>';
+  versionMsgEl.innerHTML=`${s}`;
+  procInfoSum.innerHTML=`Всего занято ${mas.length} процессами ${new Intl.NumberFormat('ru-RU').format(count_vmsime)}&nbsp;Mb. памяти. CPU:${count_cpu}%`;
   }
+ 
+
+  
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+  procInfoSum = document.querySelector("#infoproc");
   versionMsgEl = document.querySelector("#version-msg");
   cheskRun = document.querySelector("#checkrun");
-  interval=setInterval(version, 1000);
-  setInterval(meminfo, 2000);
+  msgLoadCPU=document.querySelector("#msgloadcpu");
+  interval=setInterval(version, 3000);
+  setInterval(meminfo, 4000);
+  setInterval(loadcpu, 4000);
   document
     .querySelector("#version-button").style.display = 'none';
     document
@@ -153,4 +188,16 @@ window.addEventListener("DOMContentLoaded", () => {
     document
     .querySelector("#stopversion-button")
     .addEventListener("click", () => stoptimer());
+    document
+    .querySelector("#id-button")
+    .addEventListener("click", () => paramsort=0);
+    document
+    .querySelector("#name-button")
+    .addEventListener("click", () => paramsort=1);
+    document
+    .querySelector("#mem-button")
+    .addEventListener("click", () => paramsort=2);
+    document
+    .querySelector("#cpu-button")
+    .addEventListener("click", () => paramsort=3);
 });
